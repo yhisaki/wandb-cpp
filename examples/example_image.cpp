bdd704f720d5272269d073c862a6a6da586ac797
Y.Hisaki <yhisaki31@gmail.com>
2024-02-08 21:22:43 +0900

    update image example

--- examples/example_image.cpp
+++ examples/example_image.cpp
@@ -1,15 +1,16 @@
-#include <array>
+#ifdef USE_OPENCV
 #include <opencv2/opencv.hpp>
-#include <random>
-#include <thread>
-
+#endif
 #include "wandbcpp.hpp"
 
-int main() {
+int main()
+{
+#ifdef USE_OPENCV
   wandbcpp::init({.project = "example_wandb_cpp", .tags = {"image"}});
   cv::Mat img = cv::imread("examples/data/lena.png");
   wandbcpp::log({{"image_from_array", wandbcpp::Image(img)}});
   wandbcpp::log(
       {{"image_from_path", wandbcpp::Image("examples/data/lena.png")}});
   wandbcpp::finish();
+#endif
 }
